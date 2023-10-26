02dcefbd28f9ff90da72f481bbcf3d3ef02010dd
Irina <irinadaniela.geanta@gmail.com>
2023-10-26 14:43:15 +0300

    tema13

---
+++ src/main/java/tema13/IdentifyLoginElements.java
@@ -0,0 +1,30 @@
+package tema13;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.Test;
+import utils.BaseTest;
+
+import static org.testng.Assert.assertEquals;
+
+public class IdentifyLoginElements extends BaseTest {
+
+
+    @Test
+    public void loginElement(){
+        //<em>Discover</em>
+
+        WebElement loginPopup = driver.findElement(By.linkText("Login"));
+        assertEquals(loginPopup.isDisplayed(), true);
+
+        WebElement loginField = driver.findElement(By.className("login_field"));
+        WebElement passwordField = driver.findElement(By.className("password_field"));
+        assertEquals(loginField.isDisplayed(), false);
+        assertEquals(passwordField.isDisplayed(), false);
+
+        loginPopup.click();
+        assertEquals(loginField.isDisplayed(), true);
+        assertEquals(passwordField.isDisplayed(), true);
+
+    }
+}
