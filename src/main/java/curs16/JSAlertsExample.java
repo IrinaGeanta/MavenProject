d632358d807a9166397dd1e2718d4148c0a6aff7
Irina <irinadaniela.geanta@gmail.com>
2023-11-03 20:35:24 +0200

    curs17 intermediar

--- src/main/java/curs16/JSAlertsExample.java
+++ src/main/java/curs16/JSAlertsExample.java
@@ -1,5 +1,6 @@
 package curs16;
 
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
@@ -8,7 +9,7 @@ import utils.BaseTest;
 import static org.testng.Assert.assertEquals;
 
 public class JSAlertsExample extends BaseTest {
-@Test
+@Test(priority = 1)
     public void simpleAlert() throws InterruptedException {
         driver.get("https://the-internet.herokuapp.com/javascript_alerts");
 
@@ -21,5 +22,38 @@ public class JSAlertsExample extends BaseTest {
 
     WebElement result = driver.findElement(By.cssSelector("p[id='result']"));
     assertEquals(result.getText(),"You successfully clicked an alert");
+    }
+
+    @Test(priority = 2)
+    public void confirmJSAlert() throws InterruptedException {
+        driver.findElement(By.cssSelector("button[onClick = 'jsConfirm()']")).click();
+
+        Thread.sleep(3000);
+        //tratam alerta js
+        driver.switchTo().alert().dismiss();
+
+
+        WebElement result = driver.findElement(By.cssSelector("p[id='result']"));
+        assertEquals(result.getText(),"You clicked: Cancel");
+    }
+
+    @Test(priority = 3)
+    public void promptJSAlert() throws InterruptedException {
+
+        driver.findElement(By.cssSelector("button[onClick = 'jsPrompt()']")).click();
+
+        Thread.sleep(3000);
+        //tratam alerta js
+        driver.switchTo().alert().sendKeys("Text");
+        driver.switchTo().alert().accept();
+
+        /*
+        Alert alertaJS = driver.switchTo().alert();
+        alertaJS.sendKeys("Text");
+        alertaJS.accept();
+         */
+
+        WebElement result = driver.findElement(By.cssSelector("p[id='result']"));
+        assertEquals(result.getText(),"You entered: Text");
     }
 }
--- src/main/java/utils/BaseTest.java
+++ src/main/java/utils/BaseTest.java
@@ -5,12 +5,15 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 
+import java.time.Duration;
+
 public class BaseTest {
     public WebDriver driver;
     @BeforeClass
     public void setup(){
         driver = new ChromeDriver();
         driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         driver.get("https://keybooks.ro");
     }
 
